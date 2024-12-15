d158a7aeb93d86c320ec6775cf09d78ed9552e17
bodyhassan0550 <168114870+bodyhassan0550@users.noreply.github.com>
2024-12-15 02:38:01 +0200

    final
    function to cancel and update reservations and final update

--- routes/reseervation.js
+++ routes/reseervation.js
@@ -1,9 +1,29 @@
-const express= require("express")
-const router=express.Router()
-const reservationController=require("../controller/reservations")
-const isAuth = require("../midddelware/isAuth")
-router.get("/allmovies",reservationController.getAllMovies);
+const express = require("express");
+const router = express.Router();
+const reservationController = require("../controller/reservations");
+const isAuth = require("../midddelware/isAuth");
+router.get("/allmovies", reservationController.getAllMovies);
 router.get("/movie/:movieId", reservationController.OneMovie);
-router.post("/reservation/:movieId", isAuth, reservationController.ReservedMovie);
+router.post(
+  "/reservation/:movieId",
+  isAuth,
+  reservationController.ReservedMovie
+);
+router.get(
+  "/myreservation/:reservationId",
+  isAuth,
+  reservationController.getReservation
+);
+router.get("/myreservation", isAuth, reservationController.getAllReservation);
+router.post(
+  "/cancelreservation/:reservationId",
+  isAuth,
+  reservationController.CancelReservation
+);
+router.post(
+  "/updatereservation/:reservationId",
+  isAuth,
+  reservationController.UpdateReservation
+);
 
-module.exports=router
+module.exports = router;
